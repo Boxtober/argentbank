82a02ca8e2a711f17387fa83dd86dd81ddd4da06
sabrina <sabrina@verymountain.fr>
2025-03-21 08:34:44 +0100

    add logout function on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import logo from "../../designs/img/argentBankLogo.png";
 import "./navbar.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUserCircle, faSignOut } from "@fortawesome/free-solid-svg-icons";
-
+import { logout } from "../../services/apiService";
 const Navbar = () => {
+  const token = useSelector((state) => state.auth.token);
+  const firstName = useSelector((state) => state.auth.firstName);
+  console.log("token de navbar", token);
+  console.log("firstName de navbar", firstName);
+  const handleLogout = () => {
+    dispatch(logout()); // Déclencher la déconnexion
+  };
   return (
     <nav className="main-nav">
       <NavLink to="/" className="main-nav-logo">
@@ -17,22 +25,31 @@ const Navbar = () => {
         <h1 className="sr-only">Argent Bank</h1>
       </NavLink>
       <div>
-        <NavLink
-          to="/user"
-          className={(nav) =>
-            nav.isActive ? "nav-active main-nav-item" : "main-nav-item"
-          }>
-          <FontAwesomeIcon icon={faUserCircle} />
-          Tony
-        </NavLink>
-        <NavLink
-          to="/sign-in"
-          className={(nav) =>
-            nav.isActive ? "nav-active main-nav-item" : "main-nav-item"
-          }>
-          <FontAwesomeIcon icon={faSignOut} />
-          Sign In
-        </NavLink>
+        {token ? (
+          <>
+            <NavLink
+              to="/user"
+              className={(nav) =>
+                nav.isActive ? "nav-active main-nav-item" : "main-nav-item"
+              }>
+              <FontAwesomeIcon icon={faUserCircle} />
+              {firstName}
+            </NavLink>
+            <button className="main-nav-item" onClick={handleLogout}>
+              <FontAwesomeIcon icon={faSignOut} />
+              Sign out
+            </button>
+          </>
+        ) : (
+          <NavLink
+            to="/sign-in"
+            className={(nav) =>
+              nav.isActive ? "nav-active main-nav-item" : "main-nav-item"
+            }>
+            <FontAwesomeIcon icon={faUserCircle} />
+            Sign In
+          </NavLink>
+        )}
       </div>
     </nav>
   );
@@ -42,25 +59,39 @@ export default Navbar;
 
 // import React from "react";
 // import { NavLink } from "react-router-dom";
-// import logo from "../../assets/logo.png";
+// import logo from "../../designs/img/argentBankLogo.png";
 // import "./navbar.scss";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faUserCircle, faSignOut } from "@fortawesome/free-solid-svg-icons";
 
 // const Navbar = () => {
 //   return (
 //     <nav className="main-nav">
-//       <a className="main-nav-logo">
+//       <NavLink to="/" className="main-nav-logo">
 //         <img
 //           className="main-nav-logo-image"
-//           src="src\designs\img\argentBankLogo.png"
+//           src={logo}
 //           alt="Argent Bank Logo"
 //         />
 //         <h1 className="sr-only">Argent Bank</h1>
-//       </a>
+//       </NavLink>
 //       <div>
-//         <a className="main-nav-item" href="./sign-in.html">
-//           <i className="fa fa-user-circle"></i>
+//         <NavLink
+//           to="/user"
+//           className={(nav) =>
+//             nav.isActive ? "nav-active main-nav-item" : "main-nav-item"
+//           }>
+//           <FontAwesomeIcon icon={faUserCircle} />
+//           {firstName}
+//         </NavLink>
+//         <NavLink
+//           to="/sign-in"
+//           className={(nav) =>
+//             nav.isActive ? "nav-active main-nav-item" : "main-nav-item"
+//           }>
+//           <FontAwesomeIcon icon={faSignOut} />
 //           Sign In
-//         </a>
+//         </NavLink>
 //       </div>
 //     </nav>
 //   );
